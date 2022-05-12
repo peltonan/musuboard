c8c11bacbfbe5d478bdfe3248de9e8a03e4a3789
peltonan <peltonen.annette@gmail.com>
2022-05-12 11:39:06 +0300

    CSS styles - Polaroid effect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 
 import './App.css';
+import test from './images/animeMe.jpg'
 
+/* import { polaroid } from './pictures.js'
+ */
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div class="wrapper">
+          <div class="item">
+            <div class="polaroid"><img src={test} />
+              <div class="caption">Test</div>
+            </div>
+          </div>
+        </div>
 
-      </header>
-    </div>
+      </header >
+    </div >
   );
 }
-
 export default App;
---
+++ src/pictures.js
@@ -0,0 +1,14 @@
+import test from './images/animeMe.jpg'
+
+
+export const polaroid = [
+    {
+        image: <img src={test} alt="polaroidOne"></img>,
+        caption: "Text here",
+    },
+]
+
+
+
+
+
